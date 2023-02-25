416264a67f584ff5e0421b19491780e7532d8411
Juan Ordaz Velazquez <juanordaz@Juans-iMac.local>
2023-02-25 18:27:30 -0500

    Added dependency of the tools

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# References
+* Create/ADD SSH
+  -  `https://gist.github.com/rosswd/e1afd2b0b0d515517eac`
+
+# Usefull git commands
+
+```bash
+git add -- . ':!node_modules'
+```
---
+++ main.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+snitchCloneRepos() {
+  repos=(
+    'apis'
+    'apis-fastify'
+    'composer-docker'
+    'infrastructure-terraform'
+    'mobile-nativescript-vue'
+  )
+  # TODO:
+  #   - clone outside directory.
+}
+
+main () {
+  echo ""
+}
+
+main
