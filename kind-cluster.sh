58ffc4db8f4e075de2124cce87a8463fd61619db
Juan Ordaz <juan.ordaz@Juans-Mac-mini.local>
2023-05-28 15:06:47 -0400

    Created kind config

--- kind-cluster.sh
+++ kind-cluster.sh
@@ -3,39 +3,54 @@
 # Dashboard Installation:
 # - https://istio.io/latest/docs/setup/platform-setup/kind/
 
+# Docke Memory issues:
+# https://stackoverflow.com/questions/58277794/diagnosing-high-cpu-usage-on-docker-for-mac
 PLATFORM="snitzsh"
 
 kindClusterCreate () {
-  kind create cluster --name "${PLATFORM}"
+  # Clears all cache file. For local run this:
+  docker system prune --all --force
+  kind create cluster --name="${PLATFORM}" --config="./kind-config.yaml"
+  kubectl cluster-info --context "kind-${PLATFORM}"
   kubectl config get-contexts
   kubectl config use-context kind-"${PLATFORM}"
+  # minikube start --cpus 6 --memory 15987
+  # bash ../infrastructure-helm/main.sh -a=install
 }
 
-kindClusterCreateDashboard() {
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-}
+# kindClusterCreateDashboard() {
+#   kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+# }
 
-# Create after pods are available
-kindClusterCreateServiceAccount () {
-  kubectl create serviceaccount -n kubernetes-dashboard admin-user
-}
+# # Create after pods are available
+# kindClusterCreateServiceAccount () {
+#   kubectl create serviceaccount -n kubernetes-dashboard admin-user
+# }
 
-kindClusterCreateRoleBinding () {
-  kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
-}
+# kindClusterCreateRoleBinding () {
+#   kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
+# }
 
-kindClusterGetDashboardToken () {
-  token=$(kubectl -n kubernetes-dashboard create token admin-user)
-  echo "${token}"
-}
+# kindClusterGetDashboardToken () {
+#   token=$(kubectl -n kubernetes-dashboard create token admin-user)
+#   echo "${token}"
+# }
 
-kindClusterGetPods () {
-  kubectl get pod -n kubernetes-dashboard
+# kindClusterGetPods () {
+#   kubectl get pod -n kubernetes-dashboard
+# }
+
+deleteKindCluster () {
+  # bash ../infrastructure-helm/main.sh -a=uninstall
+  kubectl get crd -oname | grep --color=never 'istio.io' | xargs kubectl delete
+  kind delete cluster --name="${PLATFORM}"
 }
 
 main () {
   kindClusterCreate
 }
+
+main
 
 # URL:
 # http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
